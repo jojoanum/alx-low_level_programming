45a8d20577187b26d1147eefec748cadfae5b386
jojoanum <anumjoseph20@gmail.com>
2023-03-28 12:09:24 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers is a go
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - helps me write charaters to c
+ * characters are to be printed
+ * Return: On Success 1
+ */
+{
+	int _putchar(char c)
+		return (write(1, &c, 1));
+}	
